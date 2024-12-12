3bbcd68a2a47c074aa5679c491dd06b329f796cd
agirard <alx87grd@gmail.com>
2024-12-12 15:25:47 -0500

    added class for pygame

--- examples/projects/pygame/double_pendulum_game.py
+++ examples/projects/pygame/double_pendulum_game.py
@@ -1,4 +1,3 @@
-
 import pygame
 
 
@@ -12,23 +11,25 @@ running = True
 dt = clock.tick(60) / 1000
 dt = clock.tick(60) / 1000
 
-#define font
+# define font
 font_size = 30
 font = pygame.font.SysFont("Futura", 30)
 
 ##############################################################################
 import numpy as np
+
 ##############################################################################
-from pyro.dynamic  import pendulum
+from pyro.dynamic import pendulum
 from pyro.kinematic import drawing
-from pyro.dynamic  import pendulum
-from pyro.control  import nonlinear
+from pyro.dynamic import pendulum
+from pyro.control import nonlinear
 from pyro.analysis import simulation
 from pyro.planning.trajectoryoptimisation import DirectCollocationTrajectoryOptimisation
+
 ##############################################################################
 
 # Dynamic system
-sys  = pendulum.DoublePendulum()
+sys = pendulum.DoublePendulum()
 
 
 sys.d1 = 0.2
@@ -39,79 +40,77 @@ sys.I2 = 2.0
 
 x = sys.x0
 t = 0.0
-score = 0.0
+score = 100.0
 last_score = 0.0
 high_score = -100000.0
+stop = False
 
 x[0] = -np.pi
 
-#Planner
+# Planner
 
-#Max/Min torque
+# Max/Min torque
 sys.u_ub[0] = +20
 sys.u_ub[1] = +20
 sys.u_lb[0] = -20
 sys.u_lb[1] = -20
 
-sys.cost_function.Q[0,0] = 100.0
-sys.cost_function.Q[0,0] = 100.0
-sys.cost_function.R[0,0] = 10.0
-sys.cost_function.R[0,0] = 10.0
+sys.cost_function.Q[0, 0] = 100.0
+sys.cost_function.Q[0, 0] = 100.0
+sys.cost_function.R[0, 0] = 10.0
+sys.cost_function.R[0, 0] = 10.0
 
-planner = DirectCollocationTrajectoryOptimisation( sys , 0.2 , 20 )
+planner = DirectCollocationTrajectoryOptimisation(sys, 0.2, 20)
 
 planner.x_start = x
-planner.x_goal  = np.array([0,0,0,0])
+planner.x_goal = np.array([0, 0, 0, 0])
 
 planner.maxiter = 500
 planner.set_linear_initial_guest(True)
 planner.compute_optimal_trajectory()
 
+# planner.show_solution()
+
 # Controller
-ctl  = nonlinear.ComputedTorqueController( sys , planner.traj )
-ctl.rbar = np.array([0,0])
-ctl.w0   = 5
+ctl = nonlinear.ComputedTorqueController(sys, planner.traj)
+ctl.rbar = np.array([0, 0])
+ctl.w0 = 5
 ctl.zeta = 1
 
 
-def px_T( domain , width = 800 , height = 800 , x_axis = 0, y_axis = 1):
+def px_T(domain, width=800, height=800, x_axis=0, y_axis=1):
 
     x_range = domain[x_axis][1] - domain[x_axis][0]
     y_range = domain[y_axis][1] - domain[y_axis][0]
-    x_center = 0.5 * ( domain[x_axis][1] + domain[x_axis][0] )
-    y_center = 0.5 * ( domain[y_axis][1] + domain[y_axis][0] )
+    x_center = 0.5 * (domain[x_axis][1] + domain[x_axis][0])
+    y_center = 0.5 * (domain[y_axis][1] + domain[y_axis][0])
 
     x_scale = width / x_range
     y_scale = height / y_range
 
-    scale = min(x_scale,y_scale)
+    scale = min(x_scale, y_scale)
 
     x_offset = 0.5 * width - x_center * scale
     y_offset = 0.5 * height - y_center * scale
 
-    T = np.array([ [ scale , 0 , x_offset ],
-                   [  0 , -scale, y_offset],
-                   [    0 , 0 , 1]])
+    T = np.array([[scale, 0, x_offset], [0, -scale, y_offset], [0, 0, 1]])
 
     return T
 
-def line2pygame( line , T ):
 
-    line_B = drawing.transform_points_2D( T , line )
+def line2pygame(line, T):
+
+    line_B = drawing.transform_points_2D(T, line)
 
     n = line.shape[0]
 
-    pg_pts =[]
+    pg_pts = []
 
     for i in range(n):
 
-        pg_pts.append( (int(line_B[i,0]), int(line_B[i,1])))
+        pg_pts.append((int(line_B[i, 0]), int(line_B[i, 1])))
 
     return pg_pts
-
-
-
-
 
 
 while running:
@@ -124,8 +123,8 @@ while running:
             joy = pygame.joystick.Joystick(event.device_index)
             print(joy.get_name())
             joysticks.append(joy)
-    
-    print('joysticks:',len(joysticks))
+
+    print("joysticks:", len(joysticks))
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("white")
@@ -141,16 +140,16 @@ while running:
         # vert_move = joystick.get_axis(1)
 
     # reset
-    if t > 10.01:
+    if t > 10.00 or stop:
         last_score = score
-        high_score = max(high_score,last_score)
-        score = 0.0
+        high_score = max(high_score, last_score)
+        score = 100.0
         x = sys.x0
         # x = np.random.normal( sys.x0, np.array([0.5,1.0,0.5,0.5]))
         x[0] = -np.pi
         # x = x + np.
         t = 0.0
-    
+
     if keys[pygame.K_w]:
         u[1] = sys.u_ub[1] * 0.5
     elif keys[pygame.K_s]:
@@ -159,79 +158,82 @@ while running:
         u[1] = 0.0
 
     for joy in joysticks:
-        u[0] = joy.get_axis(1) * ( sys.u_ub[0] - sys.u_lb[0] ) * 0.5
-        u[1] = joy.get_axis(3) * ( sys.u_ub[1] - sys.u_lb[1] ) * 0.5
+        stop = joy.get_button(3)
+        u[0] = joy.get_axis(1) * (sys.u_ub[0] - sys.u_lb[0]) * 0.5
+        u[1] = joy.get_axis(3) * (sys.u_ub[1] - sys.u_lb[1]) * 0.5
 
         if joy.get_button(0):
-            u = ctl.c( x , ctl.rbar, t )
+            u = ctl.c(x, ctl.rbar, t)
+            u = np.clip(u, sys.u_lb * 2.0, sys.u_ub * 2.0)
+
+        if joy.get_button(1):
+            u = ctl.c(x, ctl.rbar, t)
 
     if keys[pygame.K_o]:
-        u = ctl.c( x , ctl.rbar, t )
+        u = ctl.c(x, ctl.rbar, t)
 
     # Dynamic
-    x = sys.x_next( x , u , t , dt )
-    score = score - sys.cost_function.g( x , u , t ) * dt / 100
+    x = sys.x_next(x, u, t, dt)
+    score = score - sys.cost_function.g(x, u, t) * dt / 100.0
     t = t + dt
 
     # Kinematic
-    q = sys.xut2q( x , u , 0 )
+    q = sys.xut2q(x, u, 0)
 
-    print('x:',x,'u:,',u, 'dt:', dt, 't:',t)
-    print('score: %.2f' % score)
+    print("x:", x, "u:,", u, "dt:", dt, "t:", t)
+    print("score: %.2f" % score)
 
-    img = font.render( 'Score: %.2f' % score, True, (0, 0, 0))
-    screen.blit(img, (0,0))
+    img = font.render("Score: %.2f" % score, True, (0, 0, 0))
+    screen.blit(img, (0, 0))
 
-    img = font.render( 'Last score: %.2f' % last_score, True, (0, 0, 0))
-    screen.blit(img, (0,40))
+    img = font.render("Last score: %.2f" % last_score, True, (0, 0, 0))
+    screen.blit(img, (0, 40))
 
-    img = font.render( 'High score: %.2f' % high_score, True, (0, 0, 0))
-    screen.blit(img, (0,80))
+    img = font.render("High score: %.2f" % high_score, True, (0, 0, 0))
+    screen.blit(img, (0, 80))
 
-    img = font.render( 't= %.2f' % t, True, (0, 0, 0))
-    screen.blit(img, (0,120))
+    img = font.render("t= %.2f" % t, True, (0, 0, 0))
+    screen.blit(img, (0, 120))
 
     # Graphical
-    lines, _ , _ = sys.forward_kinematic_lines( q )
+    lines, _, _ = sys.forward_kinematic_lines(q)
 
-    domain = sys.forward_kinematic_domain( q )
+    domain = sys.forward_kinematic_domain(q)
 
     body = lines[1]
 
+    T = px_T(domain)
 
-    T = px_T( domain )
-
-    xyz = np.array([ body[1,0] , body[1,1] , 1.0 ])
+    xyz = np.array([body[1, 0], body[1, 1], 1.0])
     px = T @ xyz
-    pygame.draw.circle(screen, 'blue', px[0:2], 20)
+    pygame.draw.circle(screen, "blue", px[0:2], 20)
 
-    xyz = np.array([ body[2,0] , body[2,1] , 1.0 ])
+    xyz = np.array([body[2, 0], body[2, 1], 1.0])
     px = T @ xyz
-    pygame.draw.circle(screen, 'blue', px[0:2], 20)
+    pygame.draw.circle(screen, "blue", px[0:2], 20)
 
-    body_px = line2pygame( body , T )
-    pygame.draw.lines( screen, 'blue',False, body_px , width = 5  )
+    body_px = line2pygame(body, T)
+    pygame.draw.lines(screen, "blue", False, body_px, width=5)
 
-    force = sys.forward_kinematic_lines_plus(x,u,0)[0][0]
+    force = sys.forward_kinematic_lines_plus(x, u, 0)[0][0]
     if force.shape[0] > 2:
-        force_px = line2pygame( force , T )
-        pygame.draw.aalines( screen, 'red',False, force_px ) #, width = 3 )
+        force_px = line2pygame(force, T)
+        pygame.draw.aalines(screen, "red", False, force_px)  # , width = 3 )
 
-    force = sys.forward_kinematic_lines_plus(x,u,0)[0][1]
+    force = sys.forward_kinematic_lines_plus(x, u, 0)[0][1]
     if force.shape[0] > 2:
-        force_px = line2pygame( force , T )
-        pygame.draw.aalines( screen, 'red',False, force_px ) #, width = 3 )
+        force_px = line2pygame(force, T)
+        pygame.draw.aalines(screen, "red", False, force_px)  # , width = 3 )
 
-    force = sys.forward_kinematic_lines_plus(x,u,0)[0][2]
+    force = sys.forward_kinematic_lines_plus(x, u, 0)[0][2]
     if force.shape[0] > 2:
-        force_px = line2pygame( force , T )
-        pygame.draw.aalines( screen, 'red',False, force_px ) #, width = 3 )
+        force_px = line2pygame(force, T)
+        pygame.draw.aalines(screen, "red", False, force_px)  # , width = 3 )
 
-    force = sys.forward_kinematic_lines_plus(x,u,0)[0][3]
+    force = sys.forward_kinematic_lines_plus(x, u, 0)[0][3]
     if force.shape[0] > 2:
-        force_px = line2pygame( force , T )
-        pygame.draw.aalines( screen, 'red',False, force_px ) #, width = 3 )
-
+        force_px = line2pygame(force, T)
+        pygame.draw.aalines(screen, "red", False, force_px)  # , width = 3 )
 
     # flip() the display to put your work on screen
     pygame.display.flip()
---
+++ examples/projects/pygame/sys2pygame.py
@@ -0,0 +1,296 @@
+# -*- coding: utf-8 -*-
+
+##############################################################################
+import pygame
+import numpy as np
+from pyro.kinematic import drawing
+
+##############################################################################
+
+
+###############################################################################
+class InteractiveContinuousDynamicSystem:
+
+    ############################
+    def __init__(self, sys, tf=10.0, dt=0.01, ctl=None, renderer="pygame"):
+
+        self.sys = sys
+        self.dt = dt
+        self.tf = tf
+
+        self.ctl = None
+        self.renderer = renderer
+
+        # Parameters
+        self.input_scale = 1.0
+        self.input_axis_mapping = [0, 2, 1, 3, 4, 5]
+        self.dot_size = 20
+        self.linewidth = 10
+
+        self.reset_memory()
+        self.high_score = -np.inf
+        self.reset_score()
+        self.init_event()
+        self.init_renderer()
+
+    ############################
+    def reset_memory(self, t=0.0):
+
+        self.x = sys.x0
+        self.t = t
+        self.u = sys.ubar
+        self.y = sys.h(self.x, self.u, self.t)
+
+    ############################
+    def reset_score(self, last_score=0.0):
+
+        self.score = 0.0
+        self.last_score = last_score
+        self.high_score = max(self.high_score, last_score)
+
+    ############################
+    def init_event(self):
+
+        pygame.init()
+        pygame.joystick.init()
+        self.joysticks = []
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.stop = False
+
+        self.dt = self.clock.tick(60) / 1000
+
+    ############################
+    def init_renderer(self, width=800, height=800):
+
+        if self.renderer == "pygame":
+
+            self.screen = pygame.display.set_mode((width, height))
+            self.font = pygame.font.SysFont("Futura", 30)
+            self.width = width
+            self.height = height
+
+        else:
+            raise ValueError("Renderer not supported.")
+
+    ############################
+    def run(self, debug=False):
+
+        while self.running:
+
+            self.process_events()
+            self.step()
+            self.render()
+
+            if self.t > self.tf or self.stop:
+                self.reset_memory()
+                self.reset_score(self.score)
+
+            if debug:
+                print("x:", self.x, "u:,", self.u, "dt:", self.dt, "t:", self.t)
+                print("score: %.2f" % self.score)
+
+        pygame.quit()
+
+    ############################
+    def process_events(self):
+
+        self.dt = self.clock.tick(60) / 1000
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.JOYDEVICEADDED:
+                joy = pygame.joystick.Joystick(event.device_index)
+                print(joy.get_name())
+                self.joysticks.append(joy)
+
+        # Keys inputs
+        u = self.sys.ubar
+
+        keys = pygame.key.get_pressed()
+
+        for j in range(sys.ubar.shape[0]):
+
+            if j == 0:
+                if keys[pygame.K_a]:
+                    u[0] = sys.u_ub[0] * self.input_scale
+                elif keys[pygame.K_d]:
+                    u[0] = sys.u_lb[0] * self.input_scale
+                else:
+                    u[0] = 0.0
+
+            if j == 1:
+                if keys[pygame.K_w]:
+                    u[1] = sys.u_ub[1] * self.input_scale
+                elif keys[pygame.K_s]:
+                    u[1] = sys.u_lb[1] * self.input_scale
+                else:
+                    u[1] = 0.0
+
+            if j == 2:
+                if keys[pygame.K_o]:
+                    u[2] = sys.u_ub[2] * self.input_scale
+                elif keys[pygame.K_l]:
+                    u[2] = sys.u_lb[2] * self.input_scale
+                else:
+                    u[2] = 0.0
+
+            if j == 3:
+                if keys[pygame.K_i]:
+                    u[3] = sys.u_ub[3] * self.input_scale
+                elif keys[pygame.K_k]:
+                    u[3] = sys.u_lb[3] * self.input_scale
+                else:
+                    u[3] = 0.0
+
+        # Joystick inputs
+        for joy in self.joysticks:
+
+            # Stop button
+            self.stop = joy.get_button(3)
+
+            for j in sys.ubar.shape[0]:
+                input = self.input_axis_mapping[j]
+                u[j] = input * (sys.u_ub[j] - sys.u_lb[j]) * self.input_scale
+
+        # Automatic modes
+        if self.ctl is not None:
+
+            t = self.t
+            y = self.y
+
+            if joy.get_button(0):
+                u = self.ctl.c(x, self.ctl.rbar, t)
+                u = np.clip(u, sys.u_lb, sys.u_ub)
+
+            if joy.get_button(1):
+                u = self.ctl.c(x, self.ctl.rbar, t)
+
+            if keys[pygame.K_o]:
+                u = self.ctl.c(x, self.ctl.rbar, t)
+
+        # Update sys inputs
+        self.u = u
+
+    ############################
+    def step(self):
+
+        # Score update
+        self.score = (
+            self.score - self.sys.cost_function.g(self.x, self.u, self.t) * self.dt
+        )
+
+        # Dynamic
+        x_next = self.sys.x_next(self.x, self.u, self.t, self.dt)
+        self.t = self.t + self.dt
+        self.x = x_next
+
+        # Output
+        self.y = self.sys.h(self.x, self.u, self.t)
+
+    ############################
+    def render(self):
+
+        if self.renderer == "pygame":
+
+            self.screen.fill("white")
+
+            # Score
+            img = self.font.render("Score: %.2f" % self.score, True, (0, 0, 0))
+            self.screen.blit(img, (0, 0))
+
+            img = self.font.render(
+                "Last score: %.2f" % self.last_score, True, (0, 0, 0)
+            )
+            self.screen.blit(img, (0, 40))
+
+            img = self.font.render(
+                "High score: %.2f" % self.high_score, True, (0, 0, 0)
+            )
+            self.screen.blit(img, (0, 80))
+
+            img = self.font.render("t= %.2f" % self.t, True, (0, 0, 0))
+            self.screen.blit(img, (0, 120))
+
+            # Kinematic
+            q = self.sys.xut2q(self.x, self.u, self.t)
+            lines, _, _ = self.sys.forward_kinematic_lines(q)
+            domain = self.sys.forward_kinematic_domain(q)
+
+            # pixel transformation
+            T = self.px_T(domain)
+
+            for line in lines:
+                line_px = self.line2pygame(line, T)
+                pygame.draw.lines(
+                    self.screen, "blue", False, line_px, width=self.linewidth
+                )
+
+                if self.dot_size > 0:
+                    for i in range(line.shape[0]):
+                        xyz = np.array([line[i, 0], line[i, 1], 1.0])
+                        px = T @ xyz
+                        pygame.draw.circle(self.screen, "blue", px[0:2], self.dot_size)
+
+            # Lines plus
+            forces = sys.forward_kinematic_lines_plus(self.x, self.u, self.t)[0]
+
+            for force in forces:
+                if force.shape[0] > 2:
+                    force_px = self.line2pygame(force, T)
+                    pygame.draw.aalines(
+                        self.screen, "red", False, force_px  # , width=self.linewidth
+                    )
+
+            pygame.display.flip()
+
+    ################################
+    def px_T(self, domain, x_axis=0, y_axis=1):
+
+        width = self.width
+        height = self.height
+
+        x_range = domain[x_axis][1] - domain[x_axis][0]
+        y_range = domain[y_axis][1] - domain[y_axis][0]
+        x_center = 0.5 * (domain[x_axis][1] + domain[x_axis][0])
+        y_center = 0.5 * (domain[y_axis][1] + domain[y_axis][0])
+
+        x_scale = width / x_range
+        y_scale = height / y_range
+
+        scale = min(x_scale, y_scale)
+
+        x_offset = 0.5 * width - x_center * scale
+        y_offset = 0.5 * height - y_center * scale
+
+        T = np.array([[scale, 0, x_offset], [0, -scale, y_offset], [0, 0, 1]])
+
+        return T
+
+    ################################
+    def line2pygame(self, line, T):
+
+        line_B = drawing.transform_points_2D(T, line)
+
+        n = line.shape[0]
+
+        pg_pts = []
+
+        for i in range(n):
+
+            pg_pts.append((int(line_B[i, 0]), int(line_B[i, 1])))
+
+        return pg_pts
+
+
+if __name__ == "__main__":
+    """MAIN TEST"""
+
+    from pyro.dynamic import pendulum
+
+    sys = pendulum.DoublePendulum()
+
+    game = InteractiveContinuousDynamicSystem(sys)
+
+    game.run(debug=True)

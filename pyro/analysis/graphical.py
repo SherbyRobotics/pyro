fbf54336c41c59ac89a16cab1901314a5bbe2272
Alexandre Girard <alx87grd@gmail.com>
2024-12-15 14:40:47 -0500

    nice working dp for double pendulum

--- pyro/analysis/graphical.py
+++ pyro/analysis/graphical.py
@@ -11,7 +11,6 @@ import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-
 from pyro.analysis import phaseanalysis
 
 
@@ -19,60 +18,67 @@ from pyro.analysis import phaseanalysis
 #  Note: Modify here matplolib setting to fit your environment
 ###############################################################################
 
-# Use interactive backend
-try:
-    # Default usage for interactive mode
-    matplotlib.use("Qt5Agg")
-    plt.ion()  # Set interactive mode
-
-except:
-
-    try:
-        # For MacOSX
-        matplotlib.use("MacOSX")
-        plt.ion()
-
-    except:
-
-        try:
-            # For MacOSX
-            matplotlib.use("macosx")
-            plt.ion()
-
-        except:
-
-            print("Warning: Could not load validated backend mode for matplotlib")
-            print(
-                "Matplotlib list of interactive backends:",
-                matplotlib.backends.backend_registry.list_builtin(
-                    matplotlib.backends.BackendFilter.INTERACTIVE
-                ),
-            )
-            plt.ion()  # Set interactive mode
-
-
 # Default figure settings
 default_figsize = (4, 3)
 default_dpi = 250
 default_linestyle = "-"
 default_fontsize = 5
 
-# True if running in IPython, False if running the file in terminal
-if hasattr(python_system, "ps1"):
-    figure_blocking = False  # Set to not block the code when showing a figure
-else:
-    # We want to block figure to prevent the script from terminating
-    figure_blocking = True  # Set to block the code when showing a figure
-
 # Embed font type in PDF when exporting
 matplotlib.rcParams["pdf.fonttype"] = 42
 matplotlib.rcParams["ps.fonttype"] = 42
 
+# True if running in IPython, False if running the file in terminal
+if hasattr(python_system, "ps1"):
+    ipython = True
+    figure_blocking = False  # Set to not block the code when showing a figure
+else:
+    # We want to block figure to prevent the script from terminating
+    ipython = False
+    figure_blocking = True  # Set to block the code when showing a figure
 
+
+# Use interactive backend
+if ipython:
+    try:
+        # For IPython
+        matplotlib.use("Qt5Agg")
+        plt.ion()  # Set interactive mode
+
+    except:
+
+        # use default backend, usually Inline
+        plt.ion()  # Set interactive mode
+
+        print("Warning: Could not load an interactive mode for matplotlib")
+        print("Animations might not work")
+        print("See settings in pyro/pyro/analysis/graphical.py")
+        print(
+            "Matplotlib list of interactive backends:",
+            matplotlib.backends.backend_registry.list_builtin(
+                matplotlib.backends.BackendFilter.INTERACTIVE
+            ),
+        )
+
+# In terminal
+else:
+    try:
+        # For MacOS
+        matplotlib.use("MacOSX")
+        plt.ion()  # Set interactive mode
+
+    except:
+        # use default system backend
+        plt.ion()  # Set interactive mode
+
+print("\n")
+print("Pyro : automatic settings for matplotlib")
+print("------------------------------------------------------")
 print("Matplotlib backend:", plt.get_backend())
 print("Matplotlib interactive:", matplotlib.is_interactive())
 # print('Matplotlib list of interactive backends:', matplotlib.rcsetup.interactive_bk)
 print("Matplotlib figure blocking:", figure_blocking)
+print("\n")
 
 
 ###############################################################################

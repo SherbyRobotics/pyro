24f03b5a1ff202eedf629a1f18139fd53c91412d
Alexandre Girard <alx87grd@gmail.com>
2025-12-04 09:51:24 -0500

    skjhds

--- examples/demos_by_system/cartpole/cartpole_with_lqr.py
+++ examples/demos_by_system/cartpole/cartpole_with_lqr.py
@@ -26,9 +26,9 @@ sys.xbar[1] = np.pi  # Up-right position
 # Cost function
 cf = costfunction.QuadraticCostFunction.from_sys(sys)
 
-cf.Q[0, 0] = 100.0
+cf.Q[0, 0] = 1.0
 cf.Q[1, 1] = 1.0
-cf.R[0, 0] = 100
+cf.R[0, 0] = 1
 
 ctl = lqr.linearize_and_synthesize_lqr_controller(sys, cf)
 
@@ -37,8 +37,8 @@ ctl = lqr.linearize_and_synthesize_lqr_controller(sys, cf)
 cl_sys = ctl + sys
 
 cl_sys.x0[0] = -3.0
-cl_sys.x0[1] = 1.0
+cl_sys.x0[1] = 3.0
 
-cl_sys.compute_trajectory(tf=0.3, solver="euler")
+cl_sys.compute_trajectory(tf=10.0, solver="euler")
 cl_sys.plot_trajectory("xu")
 cl_sys.animate_simulation(time_factor_video=1.0)
